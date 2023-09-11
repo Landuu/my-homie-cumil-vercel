6a04fab09f1ff0a04e0f7b8cde535e68c683cf55
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2023-09-11 21:26:45 +0000

    Login test

--- api/login.py
+++ api/login.py
@@ -2,22 +2,28 @@ import sys
 sys.path.append('../utils')
 from http.server import BaseHTTPRequestHandler
 from utils.common import send_response_text, get_query_param
-from utils.twitter import create_session
+from utils.config import Config
+from tweety import Twitter
+from os.path import dirname, abspath, join
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         code = get_query_param(self.path, 'code')
-        print(code)
         
         if(code is None or len(code) != 6 or not str.isnumeric(code)):
             send_response_text(self, 'Invalid code')
             return
-        
+
         try:
-            create_session(code)
+            dir = dirname(abspath(__file__))
+            path = join(dir, '..', 'u')
+            config = Config()
+            tw = Twitter(path)
+            tw.sign_in(username=config.tw_login, password=config.tw_pass, extra=code)
         except:
             send_response_text(self, 'Login failed')
+            return
 
         send_response_text(self, 'Login success')
         return
--- api/twitter.py
+++ api/twitter.py
@@ -4,6 +4,8 @@ from http.server import BaseHTTPRequestHandler
 from utils.common import send_response_text
 from utils.twitter import create_session
 from utils.db import get_login_lock
+from tweety import Twitter
+from os.path import dirname, abspath, join
 
 
 class handler(BaseHTTPRequestHandler):
@@ -13,7 +15,16 @@ class handler(BaseHTTPRequestHandler):
             send_response_text(self, 'Twitter locked, login to refresh user session')
             return
         
-        twitter = create_session()
+        twitter = None
+        try:
+            dir = dirname(abspath(__file__))
+            path = join(dir, '..', 'u')
+            twitter = Twitter(path)
+            twitter.connect()
+        except:
+            send_response_text(self, 'Login failed')
+            return
+            
         tweets = twitter.get_tweets('2Dgirlenjoyer', replies=False)
         
         filtered = []
@@ -24,5 +35,5 @@ class handler(BaseHTTPRequestHandler):
         for f in filtered:
             print(f)
 
-        send_response_text(self, 'Hllo, twitter!')
+        send_response_text(self, 'Hllo, twitter!' + len(filtered))
         return
